907cabe68bb03d1cea48de022e7304e878079126
Tiaan Boshoff <tiaanboshoff02@gmail.com>
2023-12-07 12:32:51 -0800

    added base logger

---
+++ src/__init__.py
@@ -0,0 +1,14 @@
+import logging as _logging
+
+if not _logging.getLogger(__name__).hasHandlers():
+    _logger = _logging.getLogger(__name__)
+    _handler = _logging.StreamHandler()
+    _formatter = _logging.Formatter(
+        "%(levelname)-8s : %(name)-16s : %(message)-s"
+        )
+    
+    _logger.setLevel(_logging.DEBUG)
+    _handler.setLevel(_logging.DEBUG)
+    _handler.setFormatter(_formatter)
+    _logger.addHandler(_handler)
+    
